9eaa82eca4ef424a74ad92df2e37fa1bdd002471
SVenom <chotion427@gmail.com>
2021-12-20 18:12:41 +0530

    all done

--- index.js
+++ index.js
@@ -7,26 +7,14 @@ const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const flash = require('connect-flash');
 
-
-
-
-
-
 const dbconnec =require('./utils/database');
-
-
 const express = require('express');
+
+
 const bodyParser = require('body-parser');
-
-
 const newsRoute = require('./routes/news');
-// const recipieData = require('./routes/recipie');
-// // const msgRoute = require('./routes/msg');
-
 
 const app = express();
-
-// app.use(expressLayouts);
 
 app.use(cookieParser('CookingBlogSecure'));
 app.use(session({
@@ -38,12 +26,6 @@ app.use(flash());
 app.use(fileUpload());
 
 
-
-
-
-
-
-// app.set('views');
 app.set('layouts', './layouts/main_layouts');
 app.set('views engine', 'ejs');
 
@@ -51,16 +33,7 @@ app.use(bodyParser.urlencoded({ extended: false}));
 app.use(express.static(path.join(__dirname, "public")))
 
 
-// // app.use(msgRoute);
-// app.use(recipieData.route);
 app.use("/",newsRoute);
 
-
-
-// app.use((req, res, next) =>{
-//     res.render('404.pug');
-// })
-
-// const server = http.createServer();
 dbconnec();
 app.listen(3000 ,()=>{console.log("server is running in 3000")});
